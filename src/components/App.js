490639bc1d12251b51e2297e40d9e987e211e46f
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-01-08 15:47:36 -0500

    4- Manejando promesas (la peticion GET, la guardamos en estado y la desplegamos en la tabla )

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'; //importamos axios para hacer peticiones http
 
-import { render } from '@testing-library/react';
-
 class App extends Component {
 	constructor() {
 		super();
@@ -15,34 +13,25 @@ class App extends Component {
 
 	// cuando se monte el componente vamos a modificar el estado, por lo que hace un re render (vuelve a renderizar)
 	async componentDidMount() {
-
 		//hacemos una peticion de tipo get a la API y la respuesta es una promesa y para leerla usaremos async/await
-		const respuesta = await axios.get("https://jsonplaceholder.typicode.com/users") //retorna un promesa y cuando se resuelva esa promesa asignala a la variable
-		console.log(respuesta)
+		const respuesta = await axios.get(
+			'https://jsonplaceholder.typicode.com/users'
+		); //retorna un promesa y cuando se resuelva esa promesa asignala a la variable
+
+		// console.log(respuesta.data);
 
 		this.setState({
-			usuarios: [
-				{
-					nombre: 'Willy',
-					correo: 'willydavid1@hotmial.com',
-					enlace: 'willydavid1.github.io'
-				},
-				{
-					nombre: 'Platzi',
-					correo: 'platzi@hotmial.com',
-					enlace: 'platzi.com'
-				}
-			]
+			usuarios: respuesta.data
 		});
 	}
 
 	//un metodo que retorna una fila por cada usuario que hay en el estado
 	ponerFilas = () =>
 		this.state.usuarios.map((usuario) => (
-			<tr>
-				<td>{usuario.nombre}</td>
-				<td>{usuario.correo}</td>
-				<td>{usuario.enlace}</td>
+			<tr key={usuario.id}>
+				<td>{usuario.name}</td>
+				<td>{usuario.email}</td>
+				<td>{usuario.website}</td>
 			</tr>
 		));
 
