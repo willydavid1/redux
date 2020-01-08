1e57b88310fcf5b93246a04ca6bcba7622bb8272
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-01-08 11:30:55 -0500

    3- Ciclo de vida y axios para peticiones HTTP (usamos GET)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios'; //importamos axios para hacer peticiones http
+
 import { render } from '@testing-library/react';
 
 class App extends Component {
 	constructor() {
 		super();
 
-		// Creamos en el estado un atributo llamado usuarios que tiene un array con dos objetos
+		// inicializamos el estado con un atributo usuarios que es un array vacio
 		this.state = {
+			usuarios: []
+		};
+	}
+
+	// cuando se monte el componente vamos a modificar el estado, por lo que hace un re render (vuelve a renderizar)
+	async componentDidMount() {
+
+		//hacemos una peticion de tipo get a la API y la respuesta es una promesa y para leerla usaremos async/await
+		const respuesta = await axios.get("https://jsonplaceholder.typicode.com/users") //retorna un promesa y cuando se resuelva esa promesa asignala a la variable
+		console.log(respuesta)
+
+		this.setState({
 			usuarios: [
 				{
 					nombre: 'Willy',
@@ -19,9 +33,10 @@ class App extends Component {
 					enlace: 'platzi.com'
 				}
 			]
-		};
+		});
 	}
 
+	//un metodo que retorna una fila por cada usuario que hay en el estado
 	ponerFilas = () =>
 		this.state.usuarios.map((usuario) => (
 			<tr>
