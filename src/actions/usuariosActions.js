7d02a5e98accf4d3f0f378892ddfe95e10df658d
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-01-13 12:38:17 -0500

    12- try catch (para capturar el error en caso que suceda a la llamada a la API)

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -3,11 +3,15 @@ import { TRAER_TODOS } from "../types/usuariosTypes" // IMPORTAMOS SOLAMENTE la 
 
 //esta funcion traerTodos retorna otra función, dispatch (envio) es el que va a disparar esa llamada y va a contactar al reducer para que haga el cambio de estado
 export const traerTodos = () => async (dispatch) => {
-	//hacemos una peticion de tipo get a la API y la respuesta es una promesa y para leerla usaremos async/await
-	const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users'); //retorna un promesa y cuando se resuelva esa promesa asignala a la variable
-	
-	dispatch({
-		type: TRAER_TODOS,
-		payload: respuesta.data
-	});
+	try {
+		//hacemos una peticion de tipo get a la API y la respuesta es una promesa y para leerla usaremos async/await
+		const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users'); //retorna un promesa y cuando se resuelva esa promesa asignala a la variable
+		
+		dispatch({
+			type: TRAER_TODOS,
+			payload: respuesta.data
+		});
+	} catch (error) {
+		console.log("error:" + error.message)
+	}
 };
