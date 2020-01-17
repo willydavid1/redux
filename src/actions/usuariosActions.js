147abdd87f073fc7ecfdc3ea53846f9690516d09
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-01-17 12:18:07 -0500

    13- Escenarios asincronos (manejamos los estados de una peticion HTTP asincrona)

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,8 +1,11 @@
 import axios from "axios"; //importamos axios para hacer peticiones http
-import { TRAER_TODOS } from "../types/usuariosTypes" // IMPORTAMOS SOLAMENTE la constante TRAER_TODOS de esta ruta ../types/usuariosTypes
+import { TRAER_TODOS, CARGANDO, ERROR } from "../types/usuariosTypes" // IMPORTAMOS SOLAMENTE la constante TRAER_TODOS de esta ruta ../types/usuariosTypes
 
 //esta funcion traerTodos retorna otra función, dispatch (envio) es el que va a disparar esa llamada y va a contactar al reducer para que haga el cambio de estado
 export const traerTodos = () => async (dispatch) => {
+	dispatch({
+		type: CARGANDO,
+	})
 	try {
 		//hacemos una peticion de tipo get a la API y la respuesta es una promesa y para leerla usaremos async/await
 		const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users'); //retorna un promesa y cuando se resuelva esa promesa asignala a la variable
@@ -13,5 +16,9 @@ export const traerTodos = () => async (dispatch) => {
 		});
 	} catch (error) {
 		console.log("error:" + error.message)
+		dispatch({
+			type: ERROR,
+			payload: error.message
+		});
 	}
 };
--- src/reducers/usuariosReducers.js
+++ src/reducers/usuariosReducers.js
@@ -1,8 +1,10 @@
-import { TRAER_TODOS } from "../types/usuariosTypes" // IMPORTAMOS SOLAMENTE la constante TRAER_TODOS de esta ruta ../types/usuariosTypes
+import { TRAER_TODOS, CARGANDO, ERROR } from '../types/usuariosTypes'; // IMPORTAMOS SOLAMENTE la constante TRAER_TODOS de esta ruta ../types/usuariosTypes
 
 // inicializamos un estado
 const INITIAL_STATE = {
-	usuarios: []
+	usuarios: [],
+	cargando: false,
+	error: ''
 };
 
 //aquí se crea la función, el estado inicial, y la acción es la "tarea a realizar"
@@ -11,7 +13,17 @@ export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case TRAER_TODOS:
 			//la tarea que llegara en esta ocasión es 'traer_usuarios'
-			return { ...state, usuarios: action.payload };
+			return {
+				...state,
+				usuarios: action.payload,
+				cargando: false
+			};
+
+		case CARGANDO:
+			return { ...state, cargando: true };
+		
+		case ERROR:
+			return { ...state, error: action.payload, cargando: false };
 
 		default:
 			return state;
