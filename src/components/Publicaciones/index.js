c787470568af1c063b5a69f9f5d65eed69c17a80
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-01-18 16:34:11 -0500

    18- Compartir Reducer (conectamos el componente publicaciones para que acceda al reducer y actions creators)

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
+// conectamos este componente a redux para acceder al reducer
+import { connect } from 'react-redux';
+import * as usuariosActions from '../../actions/usuariosActions';
+
 class Publicaciones extends Component {
+	componentDidMount() {
+		// si no existen los usuarios (si no tiene mas de cero el arreglo) haz algo
+		if (!this.props.usuarios.length) {
+			this.props.traerTodos();
+		}
+	}
+
 	render() {
-		return <div>{this.props.match.params.key}</div>;
+		console.log(this.props);
+		return (
+			<div>
+				<h1>Publicaciones de </h1>
+				{this.props.match.params.key}
+			</div>
+		);
 	}
 }
 
-export default Publicaciones;
+// Recibe todos los reducers y le decimos que reducer queremos usar en este componente.
+const mapStateToProps = (reducers) => {
+	return reducers.usuariosReducer;
+};
+
+// Ya en el connect recibe la función mapStateToProps, las acciones y por ultimo nos llega por props ese reducer es decir el estado y los action creators por props
+export default connect(mapStateToProps, usuariosActions)(Publicaciones);
