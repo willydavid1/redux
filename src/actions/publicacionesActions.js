41033483b706f8b07b505252acdc24cd395813f4
Willy David Da Conceicao Lozada <willydavidlozada@gmail.com>
2020-01-22 16:58:43 -0500

    23 y 24- Inmutabilidad (le decimos al usuariosReducer tus publicaciones estan en esta casilla) y EVITAR SOBREESCRITURA (llamamos al action de traerpublicacionesPorUsuario() solo si no existe ese atributo publicaciones_key en este usuario llamamos al action)

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import { TRAER_POR_USUARIO, CARGANDO, ERROR } from '../types/publicacionesTypes'; // IMPORTAMOS SOLAMENTE las constante TRAER_TODOS de esta ruta ../types/publicacionesTypes
+import * as usuariosTypes from '../types/usuariosTypes' //importamos los types de usuarios
+
+// destructuramos y renombramos el tipo de traer todos
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes
 
 // creamos un action creator que va hacer la llamada a un único usuario, recibe por parametro key que es el índice del ítem del arreglo que se esta enviando
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
@@ -14,7 +18,24 @@ export const traerPorUsuario = (key) => async (dispatch, getState) => {
 		...publicaciones,
 		respuesta.data
 	]
+
+	// Falta decirle al usuariosReducer tus publicaciones están en esta casilla del arreglo.
+	// NECESITO SACAR LA ULTIMA CASILLA DE ESTAS PUBLICACIONES actualizadas | me da el numero de casillas que hay (1er render da cero y en el re-render hay uno) y le restp 1 para sacar la casilla
+	// Ahora tengo que actualizar los usuarios, creo un nuevo arreglo con todos estos usuarios que hay en el estado actual
+	// al usuario en especifico de ese arreglo le creare un atributo, con la posicion de su key o index.
+	const publicaciones_key = publicaciones_actualizadas.length - 1;
+	const usuarios_actualizados = [...usuarios];
+	usuarios_actualizados[key] = {
+		...usuarios[key],
+		publicaciones_key
+	}
 	
+	// dispatch para mandar los usuarios actualizados a usuariosReducer que es un arreglo con todos los usuarios solo que con un atributo llamado publicaciones_key que nos dice en que casilla del arreglo están las publicaciones del usuario
+	dispatch({
+		type: USUARIOS_TRAER_TODOS,
+		payload: usuarios_actualizados
+	});
+
 	dispatch({
 		type: TRAER_POR_USUARIO,
 		payload: publicaciones_actualizadas
--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -5,27 +5,75 @@ import { connect } from 'react-redux';
 import * as usuariosActions from '../../actions/usuariosActions';
 import * as publicacionesActions from '../../actions/publicacionesActions';
 
+import Spinner from '../General/Spinner';
+import Fatal from '../General/Fatal';
+
+
 // destructuramos y renombramos el nombre del action creator
 const { traerTodos: usuariosTraerTodos } = usuariosActions;
 const { traerPorUsuario: publicacionesTraerPorUsuario } = publicacionesActions;
 
 class Publicaciones extends Component {
 	async componentDidMount() {
+		// Destructuramos cosas del this.props y el key por cada punto hay que destructurar mas adentro
+		const {
+			usuariosTraerTodos,
+			publicacionesTraerPorUsuario,
+			match: {
+				params: { key }
+			}
+		} = this.props;
+
 		// si no existen los usuarios (si no tiene mas de cero el arreglo) haz algo
 		if (!this.props.usuariosReducer.usuarios.length) {
-			await this.props.usuariosTraerTodos();
+			await usuariosTraerTodos();
 		}
 
-		// llamamos al actions creator de publicaciones
-		this.props.publicacionesTraerPorUsuario(this.props.match.params.key);
+		// si hay un error retorna nada 
+		if (this.props.usuariosReducer.error) {
+			return
+		}
+
+		// si este atributo publicaciones_key esta en el usuariosReducer.usuarios que me esta llegando (no ejecutes esto), Si no esta ese atributo en este usuario hay que ir a ponerla
+		if (!('publicaciones_key' in this.props.usuariosReducer.usuarios[key])) {
+			publicacionesTraerPorUsuario(key);
+		}
 	}
+
+	ponerUsuario = () => {
+		// destructuro el usuarios reducers
+		const { 
+			usuariosReducer, 
+			match: { params: { key } } 
+		} = this.props
+
+		// si en el reducer de usuarios hay un error ejecuta el componente fatal
+		if (usuariosReducer.error) {
+			return <Fatal mensaje={ usuariosReducer.error } />
+		}
+
+		// si el usuarios reducer esta cargando o que usuariosReducer.usuarios.lenght no exista 
+		if ( usuariosReducer.usuarios.length === 0 || usuariosReducer.cargando) {
+			return <Spinner />
+		}
+
+		// del reducer de usuarios del usuario que estoy actualmente quiero el nombre
+		const nombre = usuariosReducer.usuarios[key].name
+
+		return (
+		<h1> Publicaciones de {nombre} </h1>
+		)
+
+	};
 
 	render() {
 		console.log(this.props);
 		return (
 			<div>
-				<h1>Publicaciones de </h1>
+				
 				{this.props.match.params.key}
+
+				{ this.ponerUsuario() }
 			</div>
 		);
 	}
