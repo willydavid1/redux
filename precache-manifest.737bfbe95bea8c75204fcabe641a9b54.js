213a6871debcac2c6071e198cade5bc24965e502
willydavid1 <willydavidlozada@gmail.com>
2020-04-14 11:48:56 -0500

    Updates

---
+++ precache-manifest.737bfbe95bea8c75204fcabe641a9b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283e92eeab834f7a66df6c8fff88d4b0",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "d523b0dca715491ed7ac",
+    "url": "/redux/static/css/main.1d3ccf9f.chunk.css"
+  },
+  {
+    "revision": "1c380e68f14b017e62c1",
+    "url": "/redux/static/js/2.aedfc709.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/redux/static/js/2.aedfc709.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d523b0dca715491ed7ac",
+    "url": "/redux/static/js/main.65942e33.chunk.js"
+  },
+  {
+    "revision": "4766740c175f0dc4e6c6",
+    "url": "/redux/static/js/runtime-main.1a7b2c7f.js"
+  }
+]);
